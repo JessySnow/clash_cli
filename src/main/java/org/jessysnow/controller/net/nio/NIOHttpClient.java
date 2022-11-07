07cf2148202e1ccb2815056ee1dde16cee64dd09
JessySnow <jessysnow12345@gmail.com>
2022-11-07 18:40:02 +0800

    RENAME -- NIOSimpleHttpClient -> NIOHttpClient

--- src/main/java/org/jessysnow/controller/net/nio/NIOHttpClient.java
+++ src/main/java/org/jessysnow/controller/net/nio/NIOHttpClient.java
@@ -4,6 +4,6 @@ package org.jessysnow.controller.net.nio;
  * It's a no blocking http client
  * mainly used to invoke a stream-http-api
  */
-public class NIOSimpleHttpClient {
+public class NIOHttpClient {
 
 }
